0c6f0fab29d5387a4062664fbf3250b49904d36d
KyleGoslan <kylegoslan@me.com>
2015-05-14 17:52:11 +0100

    Basic page added

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,17 +1,21 @@
 <!-- if you need user information, just put them into the $_SESSION variable and output them here -->
 
-<?php
-	$user = UsersQuery::create()->findPK($_SESSION["user_id"]);
-	var_dump($user);
-	echo "<br>";
+<?php $user = UsersQuery::create()->findPK($_SESSION["user_id"]); ?>
 
-	foreach($user->getEventss() as $event) {
-		var_dump($event->toArray());
-		echo "<br>";
-	}
-?>
+<div>
+	Hey, <?= $user->getUserName(); ?>. You are logged in.
+</div>
 
-Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
+<div>
+Your Events
+</div>
+<?php foreach($user->getEventss() as $event) { ?>
+	<div>
+		<h3><?= $event->getEventType(); ?></h3>
+		<p>On: <?= $event->getEventDate('Y-m-d H:i:s'); ?></p>
+	</div>
+<?php } ?>
+
 Try to close this browser tab and open it again. Still logged in! ;)
 
 <!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
