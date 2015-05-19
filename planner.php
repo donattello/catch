c54c8b7eedc44a4a83f9bedcc2502544f088f352
Martin Kiss <martin@Martins-MacBook-Pro.local>
2015-05-19 11:24:19 +0100

    created events form in planner

--- planner.php
+++ planner.php
@@ -25,6 +25,40 @@
                         <option value="9">Rugby</option>
                         <option value="10">Badminton</option>
                     </select>
+                
+                    <select name="day">
+                        <option value="1">Monday</option>
+                        <option value="2">Tuesday</option>
+                        <option value="3">Wednesday</option>
+                        <option value="4">Thursday</option>
+                        <option value="5">Friday</option>
+                        <option value="6">Saturday</option>
+                        <option value="7">Sunday</option>
+                    </select>
+                    
+                    <input type="date" name="date">
+                    
+                    <input type="time" name="usr_time">
+                   
+                    <input type="text" name="place">
+                   
+                    <select name="people">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>  
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                    </select>
                     <input type="submit" name="add-event" value="Add Event">
                 </form>
                 
--- views/createEvent.php
+++ views/createEvent.php
@@ -1,6 +1,3 @@
-<?php include ("views/_templates/header.php");
+<?php include ("views/_templates/header.php");?>
 
-
-
-
-?>
+<?php include ("views/_templates/footer.php"); ?>
